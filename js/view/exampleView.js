f885a6a09533b705e87a1b821d95761fec6071f8
hertzan <hertz.marcus@gmail.com>
2016-02-15 19:24:55 +0100

    klart för dagen

--- js/view/exampleView.js
+++ js/view/exampleView.js
@@ -6,7 +6,7 @@ var ExampleView = function (container,model) {
  this.numberOfGuests = container.find("#numberOfGuests");
  this.plusButton = container.find("#plusGuest");
  this.minusButton = container.find("#minusGuest");
+
  
  this.numberOfGuests.html(model.getNumberOfGuests());
-
 }
