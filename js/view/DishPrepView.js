d24562e05f4514a0cf52feef73868f931e7e8c35
zehuashan <zehua.shan@hotmail.com>
2016-02-16 19:04:13 +0100

    Preperations view
    css/script

--- js/view/DishPrepView.js
+++ js/view/DishPrepView.js
@@ -1,9 +1,9 @@
 //ExampleView Object constructor56
 var DishPrepView = function (container,model) {
+	container.find(".statustext span").html(model.getNumberOfGuests());
 	container.find("#dishName span").html(model.getDish(1).name);
 	container.find("#prep span").html(model.getDish(1).description);
-	container.find(".statustext span").html(model.getNumberOfGuests());
-	container.find("#menu101 span").html(model.getDishPrice(101));
-	container.find("#menu200 span").html(model.getDishPrice(200));
-	container.find("#totPrice span").html(model.getTotalMenuPrice());
+	container.find("#img1").html("<img src='" + 'images/' + model.getDish(1).image + "' alt='" + model.getDish(1).name + "'>");
+	container.find("#img2").html("<img src='" + 'images/' + model.getDish(1).image + "' alt='" + model.getDish(1).name + "'>");
+	container.find("#img3").html("<img src='" + 'images/' + model.getDish(1).image + "' alt='" + model.getDish(1).name + "'>");
 }
