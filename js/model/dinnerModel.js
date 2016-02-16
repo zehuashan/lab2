17961f5772160eefba74b0aba59434843dd76d2e
Zehua <zehua.shan@hotmail.com>
2016-02-16 15:47:52 +0100

    moar fixes
    moar model/overview script fixes

--- js/model/dinnerModel.js
+++ js/model/dinnerModel.js
@@ -3,7 +3,73 @@ var DinnerModel = function () {
 	//TODO Lab 2 implement the data structure that will hold number of guest
 	// and selected dinner options for dinner menu
 	var numberOfGuests = 4;
-	var menu = [];
+	var menu = [{
+		'id':1,
+		'name':'French toast',
+		'type':'starter',
+		'image':'toast.jpg',
+		'description':"In a large mixing bowl, beat the eggs. Add the milk, brown sugar and nutmeg; stir well to combine. Soak bread slices in the egg mixture until saturated. Heat a lightly oiled griddle or frying pan over medium high heat. Brown slices on both sides, sprinkle with cinnamon and serve hot.",
+		'ingredients':[{ 
+			'name':'eggs',
+			'quantity':0.5,
+			'unit':'',
+			'price':10
+			},{
+			'name':'milk',
+			'quantity':30,
+			'unit':'ml',
+			'price':6
+			},{
+			'name':'brown sugar',
+			'quantity':7,
+			'unit':'g',
+			'price':1
+			},{
+			'name':'ground nutmeg',
+			'quantity':0.5,
+			'unit':'g',
+			'price':12
+			},{
+			'name':'white bread',
+			'quantity':2,
+			'unit':'slices',
+			'price':2
+			}]
+		},{
+		'id':101,
+		'name':'MD 2',
+		'type':'main dish',
+		'image':'bakedbrie.jpg',
+		'description':"Here is how you make it... Lore ipsum...",
+		'ingredients':[{ 
+			'name':'ingredient 1',
+			'quantity':1,
+			'unit':'pieces',
+			'price':8
+			},{
+			'name':'ingredient 2',
+			'quantity':15,
+			'unit':'g',
+			'price':7
+			},{
+			'name':'ingredient 3',
+			'quantity':10,
+			'unit':'ml',
+			'price':4
+			}]
+		},{
+		'id':200,
+		'name':'Chocolat Ice cream',
+		'type':'dessert',
+		'image':'icecream.jpg',
+		'description':"Here is how you make it... Lore ipsum...",
+		'ingredients':[{ 
+			'name':'ice cream',
+			'quantity':100,
+			'unit':'ml',
+			'price':6
+			}]
+		}];
 
     this.plusGuest = function () {
         numberOfGuests = numberOfGuests + 1;
--- js/view/OverviewView.js
+++ js/view/OverviewView.js
@@ -1,4 +1,9 @@
 //ExampleView Object constructor56
 var OverviewView = function (container,model) {
 
+	container.find(".statustext span").html(model.getNumberOfGuests());
+	container.find("#menu1 span").html(model.getDishPrice(1));
+	container.find("#menu101 span").html(model.getDishPrice(101));
+	container.find("#menu200 span").html(model.getDishPrice(200));
+	container.find("#totPrice span").html(model.getTotalMenuPrice());
 }
