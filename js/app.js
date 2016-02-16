b4aed498eab97aefd90e72f7b5e9e99971dbb721
Zehua <zehua.shan@hotmail.com>
2016-02-16 13:58:21 +0100

    model linking in dish overview
    model linking in dish overview

--- js/app.js
+++ js/app.js
@@ -1,9 +1,14 @@
 $(function() {
 	//We instantiate our model
 	var model = new DinnerModel();
+
+	//model.addDishToMenu()
 	
 	//And create the needed controllers and views
 	var exampleView = new ExampleView($("#exampleView"), model);
-	var view2 = new View2($("#view2"), model);
+	//var view2 = new View2($("#view2"), model);
+
+	//var selectView = new SelectView($("#index"), model);
+	var overviewView = new OverviewView($("#overview"), model);
 
 });
