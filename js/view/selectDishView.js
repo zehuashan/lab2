cf73ca9e6e3317381b871b145b0835b4ed67ff7d
hertzan <hertz.marcus@gmail.com>
2016-02-22 15:52:29 +0100

    prep for lab 3

--- js/view/selectDishView.js
+++ js/view/selectDishView.js
@@ -25,3 +25,59 @@ var SelectDishView = function (container,model) {
  console.log(div);
 
 }
+
+/*<div class="rightcont">
+                    <div class="selectdish">
+                        <h4>Select dish</h4>
+                    </div>
+                    <hr>
+                <div>
+                    <div class="col-md-4">
+                        <form class="form-inline">
+                            <div class="form-group">
+                                <label for="Searchhere2" class="sr-only">Search</label>
+                                <input type="search" class="form-control" id="Searchhere2" placeholder="Enter key words">
+                            </div>
+                            <button type="submit" class="btn btn-default">Go!</button>
+                        </form>
+                    </div>
+
+                    <div class="col-md-3">
+                        <select class="form-control">
+                            <option>Starter</option>
+                            <option>Main</option>
+                            <option>Dessert</option>
+                        </select>
+                    </div>
+
+                    <hr id="line2">
+
+                    <div class="row-fluid" id="rowimg">
+                        <div class="col-md-1"></div>
+                        <div class="col-md-2" id="img1"></div>
+                        <div class="col-md-2" id="img2"></div>
+                        <div class="col-md-2" id="img3"></div>
+                        <div class="col-md-2" id="img100"></div>
+                        <div class="col-md-2" id="img200"></div>
+                        <div class="col-md-1"></div>
+                    </div>
+                    <div class="row-fluid" id="rowtext">
+                        <div class="col-md-1"></div>
+                        <div class="col-md-2" id="dish1"></div>
+                        <div class="col-md-2" id="dish2"></div>
+                        <div class="col-md-2" id="dish3"></div>
+                        <div class="col-md-2" id="dish100"></div>
+                        <div class="col-md-2" id="dish200"></div>
+                        <div class="col-md-1"></div>
+                    </div>
+                    <div class="row-fluid" id="rowPar">
+                        <div class="col-md-1"></div>
+                        <div class="col-md-2" id="para1">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla ex dui, bibendum nec dui eu, elementum feugiat nibh. Duis lectus lorem, interdum ac tellus eu, gravida commodo ipsum. </div>
+                        <div class="col-md-2" id="para2">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla ex dui, bibendum nec dui eu, elementum feugiat nibh. Duis lectus lorem, interdum ac tellus eu, gravida commodo ipsum. </div>
+                        <div class="col-md-2" id="para3">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla ex dui, bibendum nec dui eu, elementum feugiat nibh. Duis lectus lorem, interdum ac tellus eu, gravida commodo ipsum. </div>
+                        <div class="col-md-2" id="para100">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla ex dui, bibendum nec dui eu, elementum feugiat nibh. Duis lectus lorem, interdum ac tellus eu, gravida commodo ipsum. </div>
+                        <div class="col-md-2" id="para200">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla ex dui, bibendum nec dui eu, elementum feugiat nibh. Duis lectus lorem, interdum ac tellus eu, gravida commodo ipsum.</div>
+                        <div class="col-md-1"></div>
+                    </div>
+                </div>
+                </div>
--- js/view/view2.js
+++ js/view/view2.js
@@ -11,3 +11,35 @@ var View2 = function (container,model) {
  container.find("#dish100").html(model.getDish(100).name);
  container.find("#dish200").html(model.getDish(200).name);
 }
+
+
+/* <div class="leftdiv1" id="view2">
+                        <h1>My Dinner</h1>
+                        <h3 id="g1">People: <span> 2 </span></h3>
+                    <button id="minusGuest" class="btn"><span class="glyphicon glyphicon-minus"></span></button>
+                    <button id="plusGuest" class="btn"><span class="glyphicon glyphicon-plus"></span></button>
+                    </div>
+
+                    <div class="leftdiv2">
+                        <div class="col-sm-6"><p class="leftcontlefttext">Dish Name</p></div>
+                        <div class="col-sm-2"></div>
+                        <div class="col-sm-4"><p class="leftcontrighttext">Cost</p></div>
+                    </div>
+
+                    <div class="leftdiv3">      
+                        <div class="col-sm-6"><p class="leftcontlefttext">Pending</p></div>
+                        <div class="col-sm-2"></div>
+                        <div class="col-sm-4"><p class="leftcontrighttext">0.00 kr</p></div>
+                    </div>
+
+                    <div class="leftdiv4">
+                        <div class="col-sm-4"></div>
+                        <div class="col-sm-4"></div>
+                        <div class="col-sm-4"><p class="leftcontrighttext">SEK 0.00</p></div>
+                    </div>
+
+                    <div class="leftdiv5">
+                    <div class="confirmbtn">
+                      <button type="button" class="btn btn-secondary">Confirm Dinner</button>
+                    </div>
+                  </div> */
