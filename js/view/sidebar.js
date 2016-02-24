69c236e57b6a2e446a9f7c8e25b759d0e01842c2
zehuashan <zehua.shan@hotmail.com>
2016-02-24 17:37:11 +0100

    sidebar update fixes
    sidebar update fixes

--- js/view/sidebar.js
+++ js/view/sidebar.js
@@ -36,13 +36,17 @@ var Sidebar = function (container,model) {
 		this.update = function(arg){
 			this.numberOfGuests.html(model.getNumberOfGuests());
 			this.totalPrice.html(model.getTotalMenuPrice());
+			$("#leftdiv3").empty();
+		
+			for(var i = 0; i < menuList.length; i++) {
+            toHtml = '<div class="leftdivitem">';
+            toHtml += '<div class="col-sm-6"><p class="leftcontlefttext">' + menuList[i].name + '</p></div>';
+            toHtml += '<div class="col-sm-2"></div>';
+         	toHtml += '<div class="col-sm-4"><p class="leftcontrighttext">' + model.getDishPrice(menuList[i].id); + 'kr</p></div></div>';
 
+            $("#leftdiv3").append(toHtml);
+			}
 		}
-		for(var i = 0; i < menuList.length; i++) {
-				menuList[i].name;
-				getDishPrice(menuList[i].id); 
-
-        }
 	
 	}
 
