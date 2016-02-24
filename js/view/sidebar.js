678d0a80396ecf48e12257ec759ebd3a8dfc0f34
hertzan <hertz.marcus@gmail.com>
2016-02-24 13:27:29 +0100

    yo

--- js/view/sidebar.js
+++ js/view/sidebar.js
@@ -2,7 +2,7 @@
 var Sidebar = function (container,model) {
  	
  	this.container = container;
-    this.numberOfGuests = container.find(".numberOfGuests");
+    this.numberOfGuests = container.find("#numberOfGuests");
 	//this.numberOfGuests = container.find(".numberOfGuests2");
 	this.plusButton = container.find("#plusGuest");
 	this.minusButton = container.find("#minusGuest");
@@ -12,6 +12,28 @@ var Sidebar = function (container,model) {
 	this.totalMenuPrice = container.find("#totalMenuPrice");
 
 	this.totalMenuPrice.html(model.getTotalMenuPrice());
+
+	var priceToHtml = function() {   
+        var menuList = model.getFullMenu();
+        var pending = "";
+        var toHtml = "";
+        if(menuList.empty()){
+        	pending = "pending"
+        } else {
+        	pending = model.getFullMenu();
+        }
+     
+        for(var i = 0; i < menuList.length; i++) {
+            toHtml = '<div class="leftdivitem">';
+            toHtml += '<div class="col-sm-6"><p class="leftcontlefttext">' + menuList[i].name + '</p></div>';
+            toHtml += '<div class="col-sm-2"></div>';
+         	toHtml += '<div class="col-sm-4"><p class="leftcontrighttext">' + model.getDishPrice(menuList[i].id); + 'kr</p></div></div>';
+
+            $("#leftdiv3").append(toHtml);
+        }
+    }
+    priceToHtml();
     
     model.addObserver(this);
 }
+
